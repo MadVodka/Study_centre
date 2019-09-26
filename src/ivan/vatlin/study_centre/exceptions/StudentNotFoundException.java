5717fa456a304f39eab9c9dcc4a8ca8dc5fa53b7
Ivan_Vatlin <ivan_valtin@epam.com>
2019-09-26 14:45:42 +0400

    method to count hours left to study added to realized in StudentServiceImpl

---
+++ src/ivan/vatlin/study_centre/exceptions/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package ivan.vatlin.study_centre.exceptions;
+
+public class StudentNotFoundException extends Exception {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
